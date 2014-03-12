c68046e380948b0d35c4db6ddfef840823f811a3
GrahamAtkinson035 <0.035ounces@gmail.com>
2014-03-12 10:49:21 -0400

    Made function good
    Made function better

--- graham.php
+++ graham.php
@@ -1,13 +1,35 @@
 <?php
 
-function dilbert ()
+
+/**
+ * Generate base Nashoba Schedule
+ * @return      [[schedule[]],[period hours[]]]
+ */
+function genYear($startDay)
 {
-    for ($x=0; $x<100; $x++) {
-    echo ($x % 5);
-    echo "\n";
+$YTD = [0,0,0,0,0,0,0];
+//     [A,B,C,D,E,F,G]
 
+$yearSchedule = ["A"];
+    for ($daynum=$startDay; $daynum<180; $daynum++) {
+        $periods = ["A","B","C","D","E","F","G"];
+        $offset = ($daynum % 7);
+        for ($i=0; $i<7; $i++) {
+            $schedule[$i] = $periods[($offset+$i)%7].(($daynum % 8)+1);
+            $YTD[($offset+$i)%7]+=46;
+            if ($i == 4)
+            {
+                $schedule[$i] = $periods[($offset+$i)%7].(($daynum % 8)+1)."L";
+                $YTD[($offset+$i)%7]+=23;
+            }
+        }
+        $yearSchedule[$daynum] = $schedule;
     }
-
+//print_r($yearSchedule);
+//print_r($YTD);
+$output = [$yearSchedule, $YTD];
+return $output;
 }
 
-dilbert();
+print_r(genYear(0)[0]);
+print_r(genYear(0)[1]);
