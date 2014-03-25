69c51fe3caff4a40bcd1583676f690950565363e
AdamVig <adamvigneaux@gmail.com>
2014-03-25 09:30:46 -0400

    Moved site.js to index.php and deleted old file

--- schedule/index.php
+++ schedule/index.php
@@ -81,7 +81,36 @@
             <script src="../res/js/json2.js"></script>
             <script src="../res/js/moment-2.5.1.js"></script>
             <script src="../res/js/clndr.js"></script>
-            <script src="../res/js/site.js"></script>
+            <script>
+              $(document).ready( function() {
+
+                // here's some magic to make sure the dates are happening this month.
+                var thisMonth = moment().format('YYYY-MM');
+
+                var eventsArray = [
+                  { date: thisMonth + '-01', title: 'A1' },
+                  { date: thisMonth + '-20', title: 'B2' }
+                ];
+
+                clndr = $('.calendar').clndr({
+                  template: $('#template-calendar').html(),
+                  events: eventsArray
+                });
+
+                // bind month navigation to the left and right arrow keys
+                $(document).keydown( function(e) {
+                  if(e.keyCode == 37) {
+                    // left arrow
+                    clndr.back();
+                  }
+                  if(e.keyCode == 39) {
+                    // right arrow
+                    clndr.forward();
+                  }
+                });
+
+              });
+            </script>
           </div>
         </body>
       </html>
