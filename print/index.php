e69a335e99285ef47dd78ceb0aab69cdf783c655
AdamVig <adamvigneaux@gmail.com>
2014-04-07 08:25:48 -0400

    Removed filler, added template tags

--- print/index.php
+++ print/index.php
@@ -32,33 +32,20 @@
       </nav>
       <div id="content">
         <div class="container">
-          <section>
-            <div class="row">
-              <div class="col-lg-4">
-                <h4>Heading</h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, harum, ea veniam pariatur ut repudiandae ipsum veritatis qui maiores illum debitis optio sed dolore odit quibusdam soluta vel! Ab, totam, incidunt vitae autem nam error natus sequi accusantium saepe at odit voluptatum praesentium quasi odio dignissimos quas delectus dolorem harum?
-                <br>
-                <a class="btn btn-success">Go</a>
-              </div>
-              <div class="col-lg-4">
-                <h4>Heading</h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, reprehenderit quia vel ullam blanditiis iste velit nulla vitae illo ipsum molestiae vero incidunt qui quaerat minima quam amet rerum et modi perspiciatis temporibus nisi sed ad fugiat fuga ea nobis optio voluptatum voluptatem aut perferendis architecto veniam praesentium omnis quas.
-                <br>
-                <a class="btn btn-success">Go</a>
-              </div>
-              <div class="col-lg-4">
-                <h4>Heading</h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, aspernatur, assumenda, perferendis, provident saepe a maxime officiis incidunt explicabo vitae adipisci quis cum iusto nam nostrum tempora ullam asperiores ea ratione excepturi sint nisi modi distinctio eaque nulla illo quod eos voluptate placeat reiciendis soluta laudantium doloremque voluptas. Ipsum, ducimus.
-                <br>
-                <a class="btn btn-success">Go</a>
-              </div>
-            </div>
-          </section>
+          <script type="text/template">
+            
+          </script>
         </div>
       </div>
       <footer class="container">
         <?php include '../res/html/footer.html'; ?>
       </footer>
+      <script src="../res/js/underscore-min.js"></script>
+      <script>
+        $(document).ready( function() {
+          var eventsArray = <?php echo json_encode($yearSchedule); ?>;
+        });
+      </script>
     </div>
   </body>
 </html>
