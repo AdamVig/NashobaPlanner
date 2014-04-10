283822916a8ec13b973e8ee11c41a5be71f28f89
AdamVig <adamvigneaux@gmail.com>
2014-04-10 11:23:52 -0400

    Initial commit

---
+++ print/print.js
@@ -0,0 +1,11 @@
+/**
+ * Outputs a week of the schedule table cells
+ * @param  {array} events Contains five events' dates and titles
+ * @return {string}       HTML of five table cells
+ */
+function makeWeek(events) {
+	var html = "";
+	for (var i=0; i < events.length; i++) {
+		
+	}
+}
